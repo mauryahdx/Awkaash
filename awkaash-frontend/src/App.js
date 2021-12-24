06c4be944fd245a534da90279b8aad9ebf0c35e8
Surya Prakash Mourya <suryamourya@iiitdmj.ac.in>
2021-12-24 16:05:35 +0530

    resolved some of the console error

--- awkaash-frontend/src/App.js
+++ awkaash-frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
    <div className='container'>   
       <Navigation></Navigation>
-      <div className='flex'><div className=' para'><h1>Welcome to <span>Awkaash</span> .</h1> <br></br> <h2>A leave sanction software for <span>IIIT-DMJ</span>.</h2></div><img className="App-logo" src='static/images/icon.svg'></img></div>
+      <div className='flex'><div className=' para'><h1>Welcome to <span>Awkaash</span> .</h1> <br></br> <h2>A leave sanction software for <span>IIIT-DMJ</span> .</h2></div><img className="App-logo" src='static/images/icon.svg'></img></div>
    </div>
 
 
