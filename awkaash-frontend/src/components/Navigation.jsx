4924c94539955a007dbecfdf798654e8b32175d8
Surya Prakash Mourya <suryamourya@iiitdmj.ac.in>
2024-07-10 18:35:18 +0530

    changed the scss accordinginly and made folder structure clearly

--- awkaash-frontend/src/components/Navigation.jsx
+++ awkaash-frontend/src/components/Navigation.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom"
 
 const Navigation = () => {
     return (
-        <nav className="flex">
+        <nav className="nav">
             
             <Link to="/"><img  className="logo" src="static/images/awkaash_logo.svg" alt="Awkaash" /></Link>
-            <div className=" flex">
-              <Link to="/login"><a>Login</a></Link>
-              <Link to="/signup"><button>Get Started</button></Link>  
+            <div className="nav__inner">
+               <Link to="/login">Login</Link>
+               <Link to="/signup"><button>Get Started</button></Link>  
             </div>
         </nav>
     )
